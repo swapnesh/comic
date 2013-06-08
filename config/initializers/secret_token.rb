5b908fd3ce55df8b8ac8a12991314f2657c2864f
swapnesh <swapnesh20032003@gmail.com>
2013-06-08 20:11:33 +0530

    First Commit creatd static controller with an index method > rails g controller static index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comic::Application.config.secret_token = '1f8e6ac04e19826db45830e684db8b8957d23524c0b239cfa8b849ae746ad05b8356f9285e40eb059330a1227521b499c5d3f11956ae4737ccadfa0a48174b56'
